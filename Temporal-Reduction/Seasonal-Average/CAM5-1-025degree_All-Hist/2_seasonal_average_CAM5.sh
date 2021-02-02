a955ec46f4e205a02d4c4c95fd955498c216cad4
Burlen Loring <bloring@lbl.gov>
2021-02-02 13:25:30 -0800

    temporal reduction clean up seasonal cam5 ex

--- Temporal-Reduction/Seasonal-Average/CAM5-1-025degree_All-Hist/2_seasonal_average_CAM5.sh
+++ Temporal-Reduction/Seasonal-Average/CAM5-1-025degree_All-Hist/2_seasonal_average_CAM5.sh
@@ -17,20 +17,13 @@ module load teca
 set -e
 set -x
 
-# configure HDF5 file locking if on Cori (CFS)community file system
-# This is not needed on Cori Lustre scratch file system
-#export HDF5_USE_FILE_LOCKING=FALSE
-
-# prevents an abort when import'ing numpy on KNL
-export OPENBLAS_NUM_THREADS=1
-
 # make a directory for the output files
 data_dir=/global/cscratch1/sd/mwehner/machine_learning_climate_data/All-Hist/CAM5-1-0.25degree_All-Hist_est1_v3_run1/h2
 
 out_dir=CAM5-1-025degree_All-Hist_est1_v3_run1_seasonal_avg
 mkdir -p ${out_dir}
 
-# compute the daily average. change -N and -n to match the rus size.
+# compute the daily average. change -N and -n to match the run size.
 # the run size is determened by the number of output time steps. here the
 # input is 20 years of 3 hourly data, the output is seasonal, with 80 seasons.
 time srun -N 80 -n 80 \
@@ -39,5 +32,3 @@ time srun -N 80 -n 80 \
         --interval seasonal --operator average --point_arrays TS TMQ --ignore_fill_value \
         --output_file ${out_dir}/CAM5-1-025degree_All-Hist_est1_v3_seasonal_avg_%t%.nc \
         --steps_per_file 4
-
-
